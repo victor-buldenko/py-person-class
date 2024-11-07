fba113cf9539c03525d27b09bbe3c1e598b9a57d
Victor Buldenko <gookidoo@gmail.com>
2024-11-07 13:47:15 +0200

    task solved

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Person:
-    # write your code here
-    pass
+    people: dict = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = [Person(human["name"], human["age"]) for human in people]
+
+    def find_human(name: str) -> Person:
+        return next((human for human in person_list if human.name == name))
+
+    for human in people:
+        if "wife" in human and human["wife"] is not None:
+            husband = find_human(human["name"])
+            wife = find_human(human["wife"])
+            husband.wife = wife
+            wife.husband = husband
+
+    return person_list
