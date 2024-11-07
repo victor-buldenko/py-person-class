bb149bc9373538b204f64348f5a47f6181eba5f5
Victor Buldenko <gookidoo@gmail.com>
2024-11-07 14:28:33 +0200

    reworked

--- app/main.py
+++ app/main.py
@@ -4,20 +4,22 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
     person_list = [Person(human["name"], human["age"]) for human in people]
-
-    def find_human(name: str) -> Person:
-        return next((human for human in person_list if human.name == name), None)
+    dict_for_merge = {el.name: el for el in person_list}
+    Person.people |= dict_for_merge
 
     for human in people:
-        if "wife" in human and human["wife"] is not None:
-            husband = find_human(human["name"])
-            wife = find_human(human["wife"])
-            husband.wife = wife
-            wife.husband = husband
+        if human.get("wife", None) is not None:
+            husband = next(
+                (el for el in person_list if el.name == human["name"]), None)
+            wife = next(
+                (el for el in person_list if el.name == human["name"]), None)
+
+            if husband and wife:
+                husband.wife = wife
+                wife.husband = husband
 
     return person_list
